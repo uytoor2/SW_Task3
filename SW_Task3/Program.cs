6162313c6a2b01fe285c400359710975b88fada0
uytoor2 <usamayounistoor@gmail.com>
2019-09-25 11:48:53 +0300

    Add project files.

---
+++ SW_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SW_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
